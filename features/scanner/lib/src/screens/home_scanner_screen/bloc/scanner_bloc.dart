7f9135a7fbebbde440bab8daee215a5527b2c7a8
NickSigoff <mikalai.sihau@innowise.com>
2025-01-06 15:52:25 +0100

    feature/improvements: dart format

--- features/scanner/lib/src/screens/home_scanner_screen/bloc/scanner_bloc.dart
+++ features/scanner/lib/src/screens/home_scanner_screen/bloc/scanner_bloc.dart
@@ -30,13 +30,15 @@ class ScannerBloc extends Bloc<ScannerEvent, ScannerState> {
   ) async {
     List<String> pictures;
     try {
-      pictures =
-          await CunningDocumentScanner.getPictures(isGalleryImportAllowed: true) ?? <String>[];
+      pictures = await CunningDocumentScanner.getPictures(
+              isGalleryImportAllowed: true) ??
+          <String>[];
 
       if (pictures.isNotEmpty) {
         final File file = await _parseImageToPdf(pictures);
 
-        await _appRouter.push(PreviewPdfResultRoute(previewFilePath: file.path));
+        await _appRouter
+            .push(PreviewPdfResultRoute(previewFilePath: file.path));
       }
     } catch (e) {
       _appEventNotifier.notify(
@@ -53,7 +55,8 @@ class ScannerBloc extends Bloc<ScannerEvent, ScannerState> {
     for (final String path in pictures) {
       final InputImage inputImage = InputImage.fromFilePath(path);
 
-      final RecognizedText result = await _textRecognizer.processImage(inputImage);
+      final RecognizedText result =
+          await _textRecognizer.processImage(inputImage);
 
       recognizedText.add(result);
     }
